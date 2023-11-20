c9c9fcda11643847f713ac7ea02e846495ad48dd
mmalik234 <78562488+mmalik234@users.noreply.github.com>
2023-11-19 22:12:49 -0600

    Create GetHelp.js

---
+++ GetHelp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link } from "react-router-dom"
+const GetHelp = () => {
+    return (
+        <div> <h1>Welcome to our Get Help Page </h1><p>Here information will be given about how the Games Functionality {
+            <h2> Game Rules <li>You will be given a menu that displays the different sections of the game</li>You will have the option to play the game <li><li>You get to choose your age</li><li>User is given the option to select the time</li><li>User gets to play the game</li><li>User get the opportunity to seek asssistance</li><li>User Being able to access Game Rules</li><li>User click to see score</li><li>User access the About us Page</li></li></h2>  
+        }</p></div>
+     
+    );
+};
+
+export default GetHelp;        
+        
+    
+
